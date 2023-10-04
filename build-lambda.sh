2053c8657b31af5adf54ff4c2e141b30b2ff738b
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2023-10-04 15:28:08 +0300

    static?

--- build-lambda.sh
+++ build-lambda.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-docker run -it -w /app -v $PWD:/app --platform linux/x86-64 haskell:9.6.3 \
-    bash -c 'cabal update && cabal install --installdir=out'
+docker run -it -w /app -v $PWD:/app --platform linux/arm64 haskell:9.6.3 \
+    bash -c 'cabal update && cabal install --installdir=out --enable-executable-static --overwrite-policy=always'
