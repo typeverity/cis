225d630d9cd72f17296f64a23059ea58f90f4a26
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2023-10-05 12:53:26 +0300

    fic

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -9,11 +9,15 @@ const app = new cdk.App();
 const dnsStack = new DnsStack(app, "DnsStack", {
   env: {
     region: "us-east-1",
+    account: process.env.CDK_DEFAULT_ACCOUNT,
   },
+  crossRegionReferences: true,
   domainNames: ["glenda.loveistheplan.net"],
 });
 
 new InfraStack(app, "InfraStack", {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "eu-north-1" },
+  crossRegionReferences: true,
   certificate: dnsStack.certificate,
   domainNames: dnsStack.domainNames,
 });
