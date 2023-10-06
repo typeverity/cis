c13085b141c563b1dfef55bc2f8ad941421f9f5e
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2023-10-06 14:43:45 +0300

    Remove old file

--- build-lambda.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-
-docker run -it -w /app -v $PWD:/app --platform linux/arm64 haskell:9.6.3 \
-    bash -c 'cabal update && cabal install --installdir=out --enable-executable-static --overwrite-policy=always'
