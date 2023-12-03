3f939cb0edb8827d71a018f6128e446cb8434368
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2023-12-03 16:24:33 +0200

    Add tags

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -6,6 +6,11 @@ import { DnsStack } from "../lib/dns-stack";
 
 const app = new cdk.App();
 
+const tags = {
+  Application: "cisserver",
+  Environment: "development",
+};
+
 const appDomain = "glenda.loveistheplan.net";
 
 // We use CloudFront as our CDN, and it requires that our HTTPS certificate is in the us-east-1 region, so let's create a separate DNS stack there. This stack is badly named as it only creates the certificate for the given `domainNames`.
@@ -20,6 +25,7 @@ const dnsStack = new DnsStack(app, "DnsStack", {
   crossRegionReferences: true,
   // These are the domain names we want to create a certificate for. We only need one here, but certificates support multiple domain names, so this property is more general than we actually need.
   domainNames: [appDomain],
+  tags,
 });
 
 // This is our (badly named) application stack that contains our Lambda, API Gateway and CloudFront distribution. It also creates the DNS records to point to our CloudFront distribution (see `appDomain` above). The end result is that the end-user can navigate to https://glenda.loveistheplan.net/item to call our Lambda (or, more likely, to get a cached response from CloudFront).
@@ -38,4 +44,5 @@ new InfraStack(app, "InfraStack", {
   domainNames: dnsStack.domainNames,
   // This is the hosted zone where the above records will be created.
   hostedZone: dnsStack.hostedZone,
+  tags,
 });
